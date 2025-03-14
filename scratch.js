babcc324ae33d4b2902c5f2f0829fe0d1985459f
itijuku <kanjukuitijuku@gmail.com>
2025-03-14 13:00:23 +0000

    Update scratch.js

--- scratch.js
+++ scratch.js
@@ -1,6 +1,7 @@
 // https://docs.turbowarp.org/development/extensions/hello-world
 
 class test {
+    
     getInfo() {
       return {
         id: 'test',
@@ -38,7 +39,55 @@ class test {
                     defaultValue: 0,
                 },
             }
-          }
+          },
+
+          {
+            opcode: 'yzutu',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'y座標を[value]ずつ変える',
+            arguments: {
+                value : {
+                    type: Scratch.ArgumentType.NUMBER,
+                    defaultValue: 0,
+                },
+            }
+          },
+          
+          {
+            opcode: 'url',
+            blockType: Scratch.BlockType.COMMAND,
+            text: '[value]を開く',
+            arguments: {
+                value : {
+                    type: Scratch.ArgumentType.STRING,
+                    // defaultValue: 0,
+                },
+            }
+          },
+
+          {
+            opcode: 'alert',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'alertを表示[value]',
+            arguments: {
+                value : {
+                    type: Scratch.ArgumentType.STRING,
+                    // defaultValue: 0,
+                },
+            }
+          },
+
+          {
+            opcode: 'input',
+            blockType: Scratch.BlockType.COMMAND,
+            text: '入力を要求',
+          },
+
+          {
+            opcode: 'kotae',
+            blockType: Scratch.BlockType.REPORTER,
+            text: '答え',
+          },
         ]
       };
     }
@@ -52,5 +101,22 @@ class test {
     xzutu(args,util) {
         util.target.setXY(util.target.x + args.value,util.target.y);
     };
+    yzutu(args,util) {
+        util.target.setXY(util.target.x,util.target.y + args.value);
+    };
+    url(args,util) {
+        window.open(args.value);
+    };
+    alert(args,util) {
+        window.alert(args.value);
+    };
+    input(args,util) {
+        kotae = window.prompt();
+    };
+    ktoae(args,util) {
+        return kotae;
+    };
   }
+  let kotae = 0;
   Scratch.extensions.register(new test());
+
